d2496d3c4144f24fdbdf91ea4a0b108e67ccdd1c
Thiago Giammattey <giammattey@gmail.com>
2020-02-07 20:11:30 -0300

    finaliza página de apresentação do instrutor

--- instructors.js
+++ instructors.js
@@ -1,5 +1,27 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age } = require('./utils')
+
+exports.show = function(req, res) {
+	const { id } = req.params
+
+	const foundInstructor = data.instructors.find(function(instructor) {
+		return instructor.id == id
+	})
+
+	if (!foundInstructor) return res.send('Instructor not found!')
+
+	const instructor = {
+		...foundInstructor,
+		age: age(foundInstructor.birth),
+		services: foundInstructor.services.split(','),
+		created_at: new Intl.DateTimeFormat('pt-BR').format(
+			foundInstructor.created_at
+		)
+	}
+
+	return res.render('instructors/show', { instructor })
+}
 
 exports.post = function(req, res) {
 	const keys = Object.keys(req.body)
@@ -10,10 +32,21 @@ exports.post = function(req, res) {
 		}
 	}
 
-	req.body.birth = Date.parse(req.body.birth)
-	req.body.created_at = Date.now()
+	let { avatar_url, birth, name, services, gender } = req.body
 
-	data.instructors.push(req.body)
+	birth = Date.parse(req.body.birth)
+	const created_at = Date.now()
+	const id = Number(data.instructors.length + 1)
+
+	data.instructors.push({
+		id,
+		name,
+		avatar_url,
+		birth,
+		gender,
+		services,
+		created_at
+	})
 
 	fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
 		if (err) return res.send('Write file error!')
