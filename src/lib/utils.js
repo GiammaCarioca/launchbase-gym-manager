2533229e5d52deec6a3a7369dffe6724bce11cc3
Thiago Giammattey <giammattey@gmail.com>
2020-02-11 23:20:36 -0300

    adiciona banco de dados para instrutores

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-	age: function(timestamp) {
+	age(timestamp) {
 		const today = new Date()
 		const birthDate = new Date(timestamp)
 
@@ -12,7 +12,7 @@ module.exports = {
 
 		return age
 	},
-	date: function(timestamp) {
+	date(timestamp) {
 		const date = new Date(timestamp)
 
 		const year = date.getUTCFullYear()
@@ -21,7 +21,8 @@ module.exports = {
 
 		return {
 			iso: `${year}-${month}-${day}`,
-			birthDay: `${day}/${month}`
+			birthDay: `${day}/${month}`,
+			format: `${day}/${month}/${year}`
 		}
 	}
 }
